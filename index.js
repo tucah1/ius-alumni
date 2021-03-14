c989530c9bda7644054d436eaa1b3161d7fdd2bf
Harun Tucakovic <harun.tucakovic1@gmail.com>
2021-03-14 18:28:16 +0100

    cors test deployment

--- index.js
+++ index.js
@@ -7,8 +7,17 @@ const studentRouter = require('./routes/studentsRouter')
 
 const PORT = process.env.PORT || 5005
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PUT, DELETE, OPTIONS'
+    )
+    res.header('Access-Control-Allow-Headers', 'Content-Type')
+    next()
+})
 app.use(express.json({ extended: false }))
-app.use(cors({ credentials: true, origin: true }))
+//app.use(cors({ credentials: true, origin: true }))
 
 app.use('/api/admin', adminRouter)
 app.use('/api/students', studentRouter)
