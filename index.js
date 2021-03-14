6cbc10079b4e06751b42569df712e908eb0cc0d4
Harun Tucakovic <harun.tucakovic1@gmail.com>
2021-03-14 18:44:17 +0100

    *

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
         'Access-Control-Allow-Methods',
         'GET, POST, PUT, DELETE, OPTIONS'
     )
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Cache-Control')
+    res.setHeader('Access-Control-Allow-Headers', '*')
     next()
 })
 app.use(express.json({ extended: false }))
