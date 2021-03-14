2fd32d2c53253cdcfe0c83e94fb99e211717dc85
Harun Tucakovic <harun.tucakovic1@gmail.com>
2021-03-14 18:38:12 +0100

    access control allow header fix

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
         'Access-Control-Allow-Methods',
         'GET, POST, PUT, DELETE, OPTIONS'
     )
-    res.header('Access-Control-Allow-Headers', 'Content-Type')
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Cache-Control')
     next()
 })
 app.use(express.json({ extended: false }))
