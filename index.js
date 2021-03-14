db5a5e32bf386b9d26b0824f6d3c4272506e85a7
Harun Tucakovic <harun.tucakovic1@gmail.com>
2021-03-14 18:57:50 +0100

    small fix

--- index.js
+++ index.js
@@ -7,17 +7,8 @@ const studentRouter = require('./routes/studentsRouter')
 
 const PORT = process.env.PORT || 5005
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader(
-        'Access-Control-Allow-Methods',
-        'GET, POST, PUT, DELETE, OPTIONS'
-    )
-    res.setHeader('Access-Control-Allow-Headers', '*')
-    next()
-})
 app.use(express.json({ extended: false }))
-//app.use(cors({ credentials: true, origin: true }))
+app.use(cors({ credentials: true, origin: true }))
 
 app.use('/api/admin', adminRouter)
 app.use('/api/students', studentRouter)
